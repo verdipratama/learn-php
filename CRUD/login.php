b06056523b08b134ab5cba6fd199feb73db35979
Verdi Pratama <verdipratama@icloud.com>
2018-10-21 22:52:20 +0700

    + Add Cookies

--- CRUD/login.php
+++ CRUD/login.php
@@ -1,13 +1,27 @@
 <?php
     // jalankan sessionnya - biasakan jalankan dulu sessionnya taro di atas
     session_start();
+    // panggil function
+    require 'function.php';
+    // set cookie dulu sebelum session
+    if( isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
+        $id  = $_COOKIE['id'];
+        $key = $_COOKIE['key'];
+
+        // ambil username berdasarkan ID
+        $result = mysqli_query($db, "SELECT username FROM user WHERE id = $id");
+        $row = mysqli_fetch_assoc($result);
+
+        // cek cookie dan username sama apa tidak?
+        if ( $key === hash('sha256', $row['username']) ) {
+            $_SESSION["login"] = true;
+        }
+    }
     // kalo sudah login, jangan tampilkan halaman login
     if( isset($_SESSION["login"])) {
         header("Location: index.php");
         exit;
     }
-    // panggil function
-    require 'function.php';
     // cek apakah tombol submit sudah ditekan apa belum?
     if( isset($_POST["login"])) {
         // menangkap username dan password dari POST
@@ -18,12 +32,19 @@
         $result = mysqli_query($db, "SELECT * FROM user WHERE username = '$username'");
 
         // cek username -> kalo ada
-        if( mysqli_num_rows($result) === 1) { // APAKAH ada berapa baris yang dikembalikan dari fungsi di DB, JIKA HASILNYA SAMA DENGAN 1(KALO ADA)
+        if ( mysqli_num_rows($result) === 1) { // APAKAH ada berapa baris yang dikembalikan dari fungsi di DB, JIKA HASILNYA SAMA DENGAN 1(KALO ADA)
             // cek password
             $row = mysqli_fetch_assoc($result); // artinya DIDALAM FUNGSI ROW ADA datanya DI DB, ID USERNAME dan PASSWORD
             if (password_verify($password, $row["password"])) { // ngecek sebuah string sama ga dengan hashnya, kebalikan dari password hash = untuk mengacak string jd hash
                 // set session
                 $_SESSION["login"] = true; // variable session keynya login diisi boolean
+
+                // set cookies
+                if (isset($_POST["remember"])) {
+                    // set cookie
+                    setcookie('id', $row['id'], time()+60);
+                    setcookie('key', hash('sha256', $row['username']), time()+60);
+                }
                 header("Location: index.php");
                 exit;
             }
@@ -52,6 +73,8 @@
             USERNAME: <input type="text" name="username" id="username"><br>
             <label for="password"></label>
             PASSWORD: <input type="password" name="password" id="password"><br>
+            <label for="remember"></label>
+            REMEMBER <input type="checkbox" name="remember" id="remember"><br>
             <button type="submit" name="login">Login!</button>
     </form>
 </body>
