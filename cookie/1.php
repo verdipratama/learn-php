8380a45646fe7ecdbac053e789b1e400b3fb0d6c
Verdi Pratama <verdipratama@icloud.com>
2018-10-21 22:10:41 +0700

    + Add Folder Cookies

--- cookie/1.php
+++ cookie/1.php
@@ -3,8 +3,9 @@
     // Penggunaan cookie bisa mengenali user
     // Penggunaan shopping cart tanpa menghilangkan belanja yang sudah di pilih
     // Penggunaan personalisasi untuk mengetauhi perilaku dari seorang user
+    // Bisa menambahkan waktu pada cookie dengan menambahkan parameternya
     // Membuat Cookie
     // Membuat parameter key & value!
-    setcookie('nama','Ferdi Pratama');
+    setcookie('nama','Ferdi Pratama', time()+60); // set 1 menit
 
 ?>
