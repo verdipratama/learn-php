65ffb6a6384d396e20ae05749e7656d6142c2685
Verdi Pratama <verdipratama@icloud.com>
2018-10-24 12:38:49 +0700

    Testing Composer

---
+++ composer/index.php
@@ -0,0 +1,24 @@
+<?php
+	// Memanggil faker
+	require 'vendor/fzaninotto/faker/src/autoload.php';
+
+	// Instansiasi
+	$faker = faker\Factory::create("id_ID");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Daftar Penduduk 2018</title>
+</head>
+<body>
+	<h1>Data Penduduk 2018</h1>
+	<?php for ( $i = 0; $i < 10; $i++) : ?>
+		<ul>
+			<li><?php echo $faker->name; ?></li>
+			<li><?php echo $faker->address; ?></li>
+			<li><?php echo $faker->email; ?></li>
+			<li><?php echo $faker->company; ?></li>
+		</ul>
+	<?php endfor; ?>
+</body>
+</html>
