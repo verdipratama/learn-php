31007d58fe673b07bd9d06542cfe9ec933ea533f
verdipratama <verdipratama@icloud.com>
2021-01-18 17:39:29 +0700

    let it flow

--- pembulatan.php
+++ pembulatan.php
@@ -4,3 +4,4 @@ $harga_awal          = 180974;
 $harga_pembulatan    = round($harga_awal, -3); //181000
 $harga_bulat_keatas  = ceil($harga_awal / 1000) * 1000; //181000
 $harga_bulat_kebawah = floor($harga_awal / 1000) * 1000; // 180000
+echo "<br>";
