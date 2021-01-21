36a2c35b4c7db7eff984527c6cd71ce326472c89
verdipratama <verdipratama@icloud.com>
2021-01-21 21:11:53 +0700

    add str to date

---
+++ utils/string_to_date.php
@@ -0,0 +1,22 @@
+<?php
+    $str = "Pada hari kamis tanggal 21 Januari 2021";
+    if (preg_match_all("/\d{1,2} \w+ \d{4}/", $str, $match)) {
+        print_r($match);
+    } else {
+        echo "not matched";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>STRING TO DATE</title>
+</head>
+
+<body>
+</body>
+
+</html>
