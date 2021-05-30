a3b2fdb7a9b0515fb5f9b24858d75aae1262447c
verdipratama <verdipratama@icloud.com>
2021-05-30 07:24:19 +0700

    add new utils

---
+++ utils/country-code.php
@@ -0,0 +1,37 @@
+<!-- mengubah dari 0 ke +62 -->
+<?php
+    $nohp = "08562121141";
+    function hp($nohp)
+    {
+        // kadang ada penulisan no hp 0811 239 345
+        $nohp = str_replace(" ", "", $nohp);
+        // kadang ada penulisan no hp (0274) 778787
+        $nohp = str_replace("(", "", $nohp);
+        // kadang ada penulisan no hp (0274) 778787
+        $nohp = str_replace(")", "", $nohp);
+        // kadang ada penulisan no hp 0811.239.345
+        $nohp = str_replace(".", "", $nohp);
+
+        // cek apakah no hp mengandung karakter + dan 0-9
+        if (!preg_match('/[^+0-9]/', trim($nohp))) {
+            // cek apakah no hp karakter 1-3 adalah +62
+            if (substr(trim($nohp), 0, 3) == '+62') {
+                $hp = trim($nohp);
+            }
+            // cek apakah no hp karakter 1 adalah 0
+            elseif (substr(trim($nohp), 0, 1) == '0') {
+                $hp = '+62' . substr(trim($nohp), 1);
+            }
+        }
+        print $hp;
+    }
+
+    hp($nohp);
+?>
+
+<!-- Dan berikut untuk menampilkan kembali "+62" menjadi "0" -->
+<?php
+    $hp  = "+628562121141";
+    $hp0 = substr_replace($hp, '0', 0, 3);
+echo $hp0;
+?>
