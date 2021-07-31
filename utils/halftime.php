1303e4dbc4eb7253fe2cf9669f2219ed10043a90
verdipratama <verdipratama@icloud.com>
2021-07-31 10:49:21 +0700

    add new problem solved

---
+++ utils/halftime.php
@@ -0,0 +1,15 @@
+<?php
+function halfHourTimes()
+{
+    $formatter = function ($time) {
+        if ($time % 3600 == 0) {
+            return date('ga', $time);
+        } else {
+            return date('g:ia', $time);
+        }
+    };
+    $halfHourSteps = range(0, 47 * 1800, 1800);
+    return array_map($formatter, $halfHourSteps);
+}
+
+// https: //stackoverflow.com/questions/3903317/how-can-i-make-an-array-of-times-with-half-hour-intervals
